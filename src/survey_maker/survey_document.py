756f4ee57b23bf7e5d060553220051a0c83c89ab
eelco <eelcovv@gmail.com>
2018-10-12 13:26:56 +0200

    Added the last structure

--- src/survey_maker/survey_document.py
+++ src/survey_maker/survey_document.py
@@ -1,3 +1,4 @@
+import string
 from pylatex.base_classes import Environment, CommandBase
 from pylatex.package import Package
 from pylatex import Document, Section, Subsection, Command
@@ -7,7 +8,8 @@ import re
 
 from cbs_utils.misc import get_logger
 
-QUESTION_TYPES = ["quantity", "percentage"]
+QUESTION_TYPES = ["quantity", "choices", "group"]
+SPECIAL_KEYS = ("fontsize", "above")
 
 logger = get_logger(__name__)
 
@@ -70,8 +72,36 @@ class QuantityQuestion(Environment):
     content_separator = "\n"
 
 
-class ChoiseItemText(CommandBase):
+class ChoiceQuestion(Environment):
+    _latex_name = "choicequestion"
+    escape = False
+    content_separator = "\n"
+
+class ChoiceGroupQuestion(Environment):
+    _latex_name = "choicegroup"
+    escape = False
+    content_separator = "\n"
+
+class ChoiceItemText(CommandBase):
     _latex_name = "choiceitemtext"
+    escape = False
+    content_separator = "\n"
+
+
+class ChoiceItem(CommandBase):
+    _latex_name = "choiceitem"
+    escape = False
+    content_separator = "\n"
+
+
+class ChoiceLine(CommandBase):
+    _latex_name = "choiceline"
+    escape = False
+    content_separator = "\n"
+
+
+class GroupChoice(CommandBase):
+    _latex_name = "groupaddchoice"
     escape = False
     content_separator = "\n"
 
@@ -116,14 +146,21 @@ class SurveyDocument(Document):
 
         with self.create(Questionnaire(options="noinfo")):
 
+            # we can start with a info block in case that is given in the *general* section of
+            # the yaml file
             if info_items is not None:
                 self.add_info(info_items, fontsize="normalsize")
 
+            # always add a Data infomation
             self.append(AddInfo(arguments=["Date", time.strftime("%d.%m.%Y")]))
 
+            # now add all the modules with questions
             self.add_all_modules()
 
     def add_all_modules(self):
+        """
+        Add all the modules
+        """
 
         for module_key, module_properties in self.questionnaire.items():
             add_this = module_properties.get("add_this", True)
@@ -137,13 +174,14 @@ class SurveyDocument(Document):
 
     def add_module(self, module_key, module_properties):
 
-        label = module_properties["label"]
+        title = module_properties["title"]
         questions = module_properties["questions"]
         info = module_properties.get("info")
 
-        self.append(Section(title=label, label=NoEscape(label_module(module_key))))
+        self.append(Section(title=title, label=NoEscape(label_module(module_key))))
 
         if info is not None:
+            # in case a info section is given, at it at the top of the module
             self.add_info(info)
 
         for key, question_properties in questions.items():
@@ -153,35 +191,71 @@ class SurveyDocument(Document):
                 continue
 
             logger.info("Adding question {}".format(key))
-
             self.add_question(key, question_properties)
 
     def add_question(self, key, question_properties):
 
-        question = question_properties["label"]
+        question = question_properties["question"]
         question_type = question_properties.get("type", "quantity")
         info = question_properties.get("info")
+        if info:
+            above = info.get("above", False)
+        else:
+            above = False
 
         if question_type not in QUESTION_TYPES:
             logger.info("question type {} not yet implemented. Skipping".format(question_type))
             return
 
         if question_type == "quantity":
-            quantity_label = question_properties.get("quantity_label", "Aantal") + ":"
+            quantity_label = question_properties.get("quantity_label", "")
+            if quantity_label != "":
+                # append a :  in case the label of the quantity is not empty
+                quantity_label += ":"
             label_width = question_properties.get("label_width", self.global_label_width)
             box_width = question_properties.get("box_width", self.global_box_width)
             with self.create(QuantityQuestion(arguments=NoEscape(question))):
+                if info is not None and above:
+                    self.add_info(info)
                 self.add_quantity_question(key,
                                            quantity_label,
                                            label_width=label_width,
                                            box_width=box_width)
-        elif question_type == "percentage":
-            logger.info("Adding a quantity percentage")
+        elif question_type == "choices":
+            logger.info("Adding a choice question")
+            choices = question_properties.get("choices")
+            number_of_columns = question_properties.get("number_of_columns", 1)
+            with self.create(ChoiceQuestion(options=[number_of_columns],
+                                            arguments=NoEscape(question))):
+                if info is not None and above:
+                    self.add_info(info)
+                self.add_choice_question(key, choices)
+        elif question_type == "group":
+            logger.info("Adding a group question")
+            groups = question_properties["groups"]
+            choice_lines = question_properties["choicelines"]
+            with self.create(ChoiceGroupQuestion(arguments=NoEscape(question))):
+                if info is not None and above:
+                    self.add_info(info)
+                self.add_choice_group_question(key, groups, choice_lines)
         else:
-            raise AssertionError("question type not known. This should not happen")
+            raise AssertionError("question type not known. Check if type of question {} is one of "
+                                 "the following: {} ".format(key, QUESTION_TYPES))
 
-        if info is not None:
+        if info is not None and not above:
             self.add_info(info)
+
+    def add_choice_group_question(self, key, groups, choice_lines):
+
+        for grp in groups:
+            self.append(GroupChoice(NoEscape(grp)))
+
+        for cnt, line in enumerate(choice_lines):
+            char = string.ascii_lowercase[cnt]
+            line_with_char = "\\textbf{" + char + "}) " + line
+            self.append(ChoiceLine(NoEscape(line_with_char)))
+
+        self.append(Command("label", NoEscape(label_question(key))))
 
     def add_info(self, info, fontsize="footnotesize"):
         """
@@ -197,7 +271,7 @@ class SurveyDocument(Document):
         with self.create(InfoEnvironment()):
             self.write_info(info, fontsize=fontsize)
 
-    def write_info(self, info, fontsize="footnotesize", is_item=False):
+    def write_info(self, info, fontsize="footnotesize", is_item=False, info_above_items=False):
         """
         A recursive method to create a nested block of text with itemizes
 
@@ -266,7 +340,7 @@ class SurveyDocument(Document):
                     # recursively with the list or dict
                     with self.create(Itemize()):
                         self.write_info(value, is_item=True, fontsize=fsize)
-                elif key == "fontsize":
+                elif key in SPECIAL_KEYS:
                     # skip the fontsize key field
                     continue
                 else:
@@ -279,8 +353,43 @@ class SurveyDocument(Document):
         else:
             raise AssertionError("Only valid for str, dict or list")
 
+    def add_choice_question(self, key, choices=None):
+        """
+        Add a question with choices
+
+        Parameters
+        ----------
+        key: str
+            Key of the question used to create a unique reference label
+        choices: list or None
+            If None, assume Nee/Ja.
+        """
+        if choices is None:
+            choice_labels = ["Nee", "Ja"]
+        else:
+            choice_labels = choices
+
+        for choice in choice_labels:
+            self.append(ChoiceItem(NoEscape(choice)))
+
+        self.append(Command("label", NoEscape(label_question(key))))
+
     def add_quantity_question(self, key=None, quantity_label=None, label_width=None,
                               box_width=4):
+        """
+        A a question with a quantity answer to the document
+
+        Parameters
+        ----------
+        key: str
+        quantity_label: str
+        label_width: int
+        box_width: fliat
+
+        Returns
+        -------
+
+        """
         logger.debug("Adding a quantity question")
 
         if label_width is None:
@@ -288,7 +397,7 @@ class SurveyDocument(Document):
         else:
             label = "\\parbox{" + label_width + "}{" + quantity_label + "}"
 
-        self.append(ChoiseItemText(arguments=["1.2em", box_width, NoEscape(label)]))
+        self.append(ChoiceItemText(arguments=["1.2em", box_width, NoEscape(label)]))
         self.append(Command("label", NoEscape(label_question(key))))
 
     def add_percentage_question(self):
